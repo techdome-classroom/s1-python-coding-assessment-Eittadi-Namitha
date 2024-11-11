f576f9db76401ba98295b3d0d8d3f6d0bca9c130
Eittadi Namitha <namithareddyeittadi@gmail.com>
2024-11-11 13:37:34 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,7 +4,6 @@ class Solution:
         if not grid:
             return 0
     #    write your code here        
-        #return 0
         rows, cols = len(grid), len(grid[0])
         visited = set()
         def dfs(r, c):
