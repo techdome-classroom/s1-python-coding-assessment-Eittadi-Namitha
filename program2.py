d4aa5e10a7831144121e4c36b8d41ff2e3064461
Eittadi Namitha <namithareddyeittadi@gmail.com>
2024-11-11 13:32:13 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -2,7 +2,8 @@ def decode_message( s: str, p: str) -> bool:
         m, n=len(s), len(p)
         dp=[[False]*(n+1) for _ in range(m+1)]
         dp[0][0]=True
-        for j in range(1,n+1)
+        for j in range(1,n+1):
+                if p
 
 # write your code here
   
