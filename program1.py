34b19d9c4e142f0c62eca3f8346988cc626a3547
Eittadi Namitha <namithareddyeittadi@gmail.com>
2024-11-11 13:37:19 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -7,19 +7,15 @@ class Solution:
         #return 0
         rows, cols = len(grid), len(grid[0])
         visited = set()
-        
         def dfs(r, c):
-        
             if (r < 0 or r >= rows or c < 0 or c >= cols or 
                 (r, c) in visited or grid[r][c] == 'W'):
                 return
-
             visited.add((r, c))
             dfs(r - 1, c) 
             dfs(r + 1, c)  
             dfs(r, c - 1) 
             dfs(r, c + 1) 
-        
         island_count = 0
         for r in range(rows):
             for c in range(cols):
