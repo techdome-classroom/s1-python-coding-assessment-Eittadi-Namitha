438cc8866bf4b23a1719be8488ea8757c346b4b0
Eittadi Namitha <namithareddyeittadi@gmail.com>
2024-11-11 13:33:57 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -7,7 +7,7 @@ def decode_message( s: str, p: str) -> bool:
                         dp[0][j]=dp[0][j-2]
         for i in range(1, m+1):
                 for j in range(1, n+1):
-                        if p[j]
+                        if p[j-1]
 
 # write your code here
   
